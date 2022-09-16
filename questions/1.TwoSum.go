d07002924341f7b5844a7becdcb29ac524d8dc79
manato kuroda <a7108100@gmail.com>
2022-09-16 17:12:08 +0900

    feat: add TwoSum.go

---
+++ questions/1.TwoSum.go
@@ -0,0 +1,18 @@
+package questions
+
+func twoSum(nums []int, target int) []int {
+	m := map[int]int{}
+	var r []int
+
+	for i, num := range nums {
+		diff := target - num
+
+		v, ok := m[diff]
+		if ok {
+			return []int{v, i}
+		}
+		m[num] = i
+	}
+
+	return r
+}
