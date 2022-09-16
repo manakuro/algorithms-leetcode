b425f3ec4d1bd23371329ba5e9a8a0be50536cb4
manato kuroda <a7108100@gmail.com>
2022-09-16 13:14:27 +0900

    feat: add 5.LongestPalindromicSubstring

---
+++ questions/5.LongestPalindromicSubstring.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const longestPalindrome = function (s) {
+  let result = ''
+  let length = 0
+
+  for (let i = 0; i < s.length; i++) {
+    let left = i
+    let right = i
+
+    // odd length
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      if (right - left + 1 > length) {
+        result = s.slice(left, right + 1)
+        length = right - left + 1
+      }
+      left--
+      right++
+    }
+
+    // even length
+    left = i
+    right = i + 1
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      if (right - left + 1 > length) {
+        result = s.slice(left, right + 1)
+        length = right - left + 1
+      }
+      left--
+      right++
+    }
+  }
+
+  return result
+}
+console.log(longestPalindrome('babad'))
+console.log(longestPalindrome('cbbd'))
+console.log(longestPalindrome('a'))
