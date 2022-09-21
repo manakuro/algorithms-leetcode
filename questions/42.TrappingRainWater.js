ff1d4fa39d79e863377481f51ca01f203cba0fc3
manato kuroda <a7108100@gmail.com>
2022-09-22 08:36:46 +0900

    feat: add 42.TrappingRainWater

---
+++ questions/42.TrappingRainWater.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const trap = function (height) {
+  if (!height.length) return 0
+
+  let left = 0
+  let right = height.length - 1
+  let leftMax = height[left]
+  let rightMax = height[right]
+  let result = 0
+
+  while (left < right) {
+    if (leftMax < rightMax) {
+      left++
+      leftMax = Math.max(leftMax, height[left])
+      result += leftMax - height[left]
+    } else {
+      right--
+      rightMax = Math.max(rightMax, height[right])
+      result += rightMax - height[right]
+    }
+  }
+
+  return result
+}
+
+console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
