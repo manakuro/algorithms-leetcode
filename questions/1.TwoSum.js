8e2dbb274c44a9d1f3430ee04189344f462198bb
manato kuroda <a7108100@gmail.com>
2022-09-21 14:07:56 +0900

    feat: add TwoSum

---
+++ questions/1.TwoSum.js
@@ -0,0 +1,14 @@
+function twoSum(nums, target) {
+  const map = new Map()
+
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i]
+    const diff = target - n
+    if (map.has(diff)) return [map.get(diff), i]
+
+    map.set(n, i)
+  }
+
+  return []
+}
+console.log(twoSum([2, 7, 11, 15], 9))
