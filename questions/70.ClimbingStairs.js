50200f9829fddb7b061d566d1705bda40fd73821
manato kuroda <a7108100@gmail.com>
2022-09-23 14:56:10 +0900

    chore: add comment

--- questions/70.ClimbingStairs.js
+++ questions/70.ClimbingStairs.js
@@ -8,6 +8,16 @@ const climbStairs = function (n) {
   let two = 1
   let temp
 
+  // e.g. n = 5
+  //  0 1 2 3 4 5
+  // |8|5|3|2|1|1|
+  //
+  // 1 + 0 = 1 path // 5
+  // 1 + 0 = 1 path // 4
+  // 1 + 1 = 2 path // 3
+  // 2 + 1 = 3 path // 2
+  // 3 + 2 = 5 path // 1
+  // 5 + 3 = 8 path // 0
   for (let i = 0; i < n - 1; i++) {
     temp = one
     one = one + two
@@ -16,4 +26,4 @@ const climbStairs = function (n) {
   return one
 }
 
-console.log(climbStairs(5))
+console.log(climbStairs(5)) // 8
