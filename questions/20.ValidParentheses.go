1aff5423d32b49ad3257f4a54d37d77b67423491
manato kuroda <a7108100@gmail.com>
2022-09-27 08:26:42 +0900

    feat: add 20.ValidParentheses

---
+++ questions/20.ValidParentheses.go
@@ -0,0 +1,26 @@
+package questions
+
+func isValid(s string) bool {
+	var stack []string
+	obj := map[string]string{
+		"(": ")",
+		"{": "}",
+		"[": "]",
+	}
+
+	for _, char := range s {
+		mapped, ok := obj[string(char)]
+		if ok {
+			stack = append(stack, mapped)
+			continue
+		}
+		if len(stack) > 0 && stack[len(stack)-1] == string(char) {
+			stack = stack[:len(stack)-1]
+			continue
+		}
+
+		return false
+	}
+
+	return len(stack) == 0
+}
