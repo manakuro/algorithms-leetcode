674906afb8168bcc327dccfbbcc1fcba06aec031
manato kuroda <a7108100@gmail.com>
2022-10-06 16:17:03 +0900

    feat: add backtracking

--- src/backtracking/subsets.js
+++ src/backtracking/subsets.js
@@ -25,4 +25,4 @@ const subsets = function (nums) {
 
   return result
 }
-console.log(subsets([1, 2, 3]))
+console.log(subsets([1, 2, 3])) // [ [ 1, 2, 3 ], [ 1, 2 ], [ 1, 3 ], [ 1 ], [ 2, 3 ], [ 2 ], [ 3 ], [] ]
