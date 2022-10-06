c78b0d230255b8164d22476a88df0d54ae0d1406
manato kuroda <a7108100@gmail.com>
2022-10-06 13:53:07 +0900

    feat: add util

--- src/util.js
+++ src/util.js
@@ -1,3 +1,13 @@
 export const swap = (arr, index1, index2) => {
   ;[arr[index1], arr[index2]] = [arr[index2], arr[index1]]
 }
+
+export const print = (head) => {
+  const arr = []
+  let current = head
+  while (current) {
+    arr.push(current.val)
+    current = current.next
+  }
+  return arr
+}
