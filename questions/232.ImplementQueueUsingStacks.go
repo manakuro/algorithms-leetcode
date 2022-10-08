f60c764391cd8ced86175a97fff79eb46e43e24c
manato kuroda <a7108100@gmail.com>
2022-10-08 10:16:58 +0900

    feat: add 232.ImplementQueueUsingStacks.go

---
+++ questions/232.ImplementQueueUsingStacks.go
@@ -0,0 +1,27 @@
+package questions
+
+type MyQueue struct {
+	Values []int
+}
+
+func Constructor() MyQueue {
+	return MyQueue{}
+}
+
+func (this *MyQueue) Push(x int) {
+	this.Values = append(this.Values, x)
+}
+
+func (this *MyQueue) Pop() int {
+	v := this.Values[0]
+	this.Values = this.Values[1:]
+	return v
+}
+
+func (this *MyQueue) Peek() int {
+	return this.Values[0]
+}
+
+func (this *MyQueue) Empty() bool {
+	return len(this.Values) == 0
+}
