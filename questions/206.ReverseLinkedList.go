2caa256e0a5f408eac06541e848314024a796d8c
manato kuroda <a7108100@gmail.com>
2022-10-11 08:36:18 +0900

    feat: add 206.ReverseLinkedList.go

---
+++ questions/206.ReverseLinkedList.go
@@ -0,0 +1,16 @@
+package questions
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	var next *ListNode
+	current := head
+
+	for current != nil {
+		next = current.Next
+		current.Next = prev
+		prev = current
+		current = next
+	}
+
+	return prev
+}
