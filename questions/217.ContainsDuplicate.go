6a4a1299d06fd2e4a985d6e327e4789e9e65a26e
manato kuroda <a7108100@gmail.com>
2022-10-17 08:15:40 +0900

    feat: add 217.ContainsDuplicate.go

---
+++ questions/217.ContainsDuplicate.go
@@ -0,0 +1,14 @@
+package questions
+
+func containsDuplicate(nums []int) bool {
+	mappings := make(map[int]struct{})
+
+	for _, num := range nums {
+		if _, ok := mappings[num]; ok {
+			return true
+		}
+		mappings[num] = struct{}{}
+	}
+
+	return false
+}
