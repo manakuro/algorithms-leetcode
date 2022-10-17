6583ff37a2ccf12f0e88e6f44a190c9286eeac71
manato kuroda <a7108100@gmail.com>
2022-10-18 08:55:15 +0900

    feat: add 53.MaximumSubarray.go

---
+++ questions/53.MaximumSubarray.go
@@ -0,0 +1,19 @@
+package questions
+
+import "math"
+
+func maxSubArray(nums []int) int {
+	max := nums[0]
+	current := 0
+
+	for _, num := range nums {
+		if current < 0 {
+			current = 0
+		}
+
+		current += num
+		max = int(math.Max(float64(max), float64(current)))
+	}
+
+	return max
+}
