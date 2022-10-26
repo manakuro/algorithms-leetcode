0c7e2679296256366c7f9065a682a02dbaf572a1
manato kuroda <a7108100@gmail.com>
2022-10-27 08:52:04 +0900

    feat: add 208.ImplementTrie(Prefix Tree).go

---
+++ questions/208.ImplementTrie(Prefix Tree).go
@@ -0,0 +1,54 @@
+package questions
+
+type TrieNode struct {
+	Children    map[string]*TrieNode
+	isEndOfWord bool
+}
+
+type Trie struct {
+	root *TrieNode
+}
+
+func (t *Trie) Insert(word string) {
+	current := t.root
+
+	for _, char := range word {
+		_, ok := current.Children[string(char)]
+		if !ok {
+			current.Children[string(char)] = &TrieNode{
+				Children:    make(map[string]*TrieNode),
+				isEndOfWord: false,
+			}
+		}
+		current = current.Children[string(char)]
+	}
+	current.isEndOfWord = true
+}
+
+func (t *Trie) Search(word string) bool {
+	current := t.root
+
+	for _, char := range word {
+		_, ok := current.Children[string(char)]
+		if !ok {
+			return false
+		}
+		current = current.Children[string(char)]
+	}
+
+	return current.isEndOfWord
+}
+
+func (t *Trie) StartsWith(prefix string) bool {
+	current := t.root
+
+	for _, p := range prefix {
+		_, ok := current.Children[string(p)]
+		if !ok {
+			return false
+		}
+		current = current.Children[string(p)]
+	}
+
+	return true
+}
