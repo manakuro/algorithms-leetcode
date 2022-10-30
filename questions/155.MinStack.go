2afed8fee828ac9ad509a536ce73f9f125f8bbdc
manato kuroda <a7108100@gmail.com>
2022-10-30 09:08:58 +0900

    feat: add 155.MinStack.go

---
+++ questions/155.MinStack.go
@@ -0,0 +1,40 @@
+package questions
+
+import "math"
+
+type MinStack struct {
+	Stack    []int
+	MinStack []int
+}
+
+func (m *MinStack) Push(val int) {
+	m.Stack = append(m.Stack, val)
+
+	minStackValue := val
+	if len(m.MinStack) > 0 {
+		minStackValue = int(math.Min(float64(m.MinStack[len(m.MinStack)-1]), float64(val)))
+	}
+
+	m.MinStack = append(m.MinStack, minStackValue)
+}
+
+func (m *MinStack) Pop() {
+	m.Stack = m.Stack[:len(m.Stack)-1]
+	m.MinStack = m.MinStack[:len(m.MinStack)-1]
+}
+
+func (m *MinStack) Top() int {
+	if len(m.Stack) == 0 {
+		return 0
+	}
+
+	return m.Stack[len(m.MinStack)-1]
+}
+
+func (m *MinStack) GetMin() int {
+	if len(m.MinStack) == 0 {
+		return 0
+	}
+
+	return m.MinStack[len(m.MinStack)-1]
+}
