223bc2284532c2c3048cecab104d2cc32766abab
manato kuroda <a7108100@gmail.com>
2022-11-04 09:42:44 +0900

    feat: add 39.CombinationSum.go

---
+++ questions/39.CombinationSum.go
@@ -0,0 +1,27 @@
+package questions
+
+func combinationSum(candidates []int, target int) [][]int {
+	var result [][]int
+
+	var traverse func(i int, current []int, total int)
+	traverse = func(i int, current []int, total int) {
+		if total == target {
+			result = append(result, append([]int{}, current...))
+			return
+		}
+
+		if i >= len(candidates) || total > target {
+			return
+		}
+
+		current = append(current, candidates[i])
+		traverse(i, current, total+candidates[i])
+
+		current = current[:len(current)-1]
+		traverse(i+1, current, total)
+	}
+
+	traverse(0, []int{}, 0)
+
+	return result
+}
