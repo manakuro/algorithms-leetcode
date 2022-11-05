849b58a4a99cc3f46fb526847d502e23dd3f7911
manato kuroda <a7108100@gmail.com>
2022-11-05 09:06:41 +0900

    feat: add 46.Permutations.go

---
+++ questions/46.Permutations.go
@@ -0,0 +1,25 @@
+package questions
+
+func permute(nums []int) [][]int {
+	var result [][]int
+
+	if len(nums) == 1 {
+		return [][]int{append([]int{}, nums...)}
+	}
+
+	for i := 0; i < len(nums); i++ {
+		num := nums[0]
+		nums = nums[1:]
+
+		perms := permute(nums)
+
+		for j := range perms {
+			perms[j] = append(perms[j], num)
+		}
+
+		result = append(result, perms...)
+		nums = append(nums, num)
+	}
+
+	return result
+}
