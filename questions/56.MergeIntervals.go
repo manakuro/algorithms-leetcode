98ea661a6d8fb30fd6f585b230a236e7014ab426
manato kuroda <a7108100@gmail.com>
2022-11-06 09:32:59 +0900

    feat: add 56.MergeIntervals.go

---
+++ questions/56.MergeIntervals.go
@@ -0,0 +1,29 @@
+package questions
+
+import (
+	"math"
+	"sort"
+)
+
+func mergeIntervals(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	result := [][]int{
+		intervals[0],
+	}
+	for _, interval := range intervals[1:] {
+		start, end := interval[0], interval[1]
+		lastEnd := result[len(result)-1][1]
+		isOverlapping := start <= lastEnd
+
+		if isOverlapping {
+			result[len(result)-1][1] = int(math.Max(float64(lastEnd), float64(end)))
+		} else {
+			result = append(result, []int{start, end})
+		}
+	}
+
+	return result
+}
