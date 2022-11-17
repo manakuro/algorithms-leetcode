e694906545c499a88d1c5cbfd8ca222d7b1f4bb5
manato kuroda <a7108100@gmail.com>
2022-11-18 08:39:33 +0900

    feat: add 11.ContainerWithMostWater.go

---
+++ questions/11.ContainerWithMostWater.go
@@ -0,0 +1,23 @@
+package questions
+
+import "math"
+
+func maxArea(height []int) int {
+	result := 0
+	left := 0
+	right := len(height) - 1
+
+	for left < right {
+		area := (right - left) * int(math.Min(float64(height[left]), float64(height[right])))
+		result = int(math.Max(float64(result), float64(area)))
+
+		if height[left] < height[right] {
+			left++
+			continue
+		}
+
+		right--
+	}
+
+	return result
+}
