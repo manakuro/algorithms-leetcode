0234cf22244d2d38f9ef0f168687ff1265d0ce24
manato kuroda <a7108100@gmail.com>
2022-11-19 09:26:42 +0900

    feat: add 17.LetterCombinationsOfAPhoneNumber.go

---
+++ questions/17.LetterCombinationsOfAPhoneNumber.go
@@ -0,0 +1,42 @@
+package questions
+
+func letterCombinations(digits string) []string {
+	if len(digits) == 0 {
+		return []string{}
+	}
+
+	phone := map[string][]string{
+		"2": {"a", "b", "c"},
+		"3": {"d", "e", "f"},
+		"4": {"g", "h", "i"},
+		"5": {"j", "k", "l"},
+		"6": {"m", "n", "o"},
+		"7": {"p", "q", "r", "s"},
+		"8": {"t", "u", "v"},
+		"9": {"w", "x", "y", "z"},
+	}
+
+	var result []string
+	var dfs func(i int, current string)
+
+	dfs = func(i int, current string) {
+		if len(current) == len(digits) {
+			result = append(result, current)
+		}
+
+		if i >= len(digits) {
+			return
+		}
+
+		digit := string(digits[i])
+		letters, _ := phone[digit]
+
+		for _, letter := range letters {
+			dfs(i+1, current+letter)
+		}
+	}
+
+	dfs(0, "")
+
+	return result
+}
