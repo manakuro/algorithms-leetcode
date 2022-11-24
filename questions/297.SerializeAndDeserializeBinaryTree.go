68f65c315a843c6dbcb215e1548a8e0d85e358f9
manato kuroda <a7108100@gmail.com>
2022-11-25 08:35:29 +0900

    feat: add 297.SerializeAndDeserializeBinaryTree.go

---
+++ questions/297.SerializeAndDeserializeBinaryTree.go
@@ -0,0 +1,55 @@
+package questions
+
+import (
+	"strconv"
+	"strings"
+)
+
+type Codec struct {
+}
+
+// Serializes a tree to a single string.
+func (c *Codec) serialize(root *TreeNode) string {
+	var traverse func(node *TreeNode)
+
+	var result []string
+
+	traverse = func(node *TreeNode) {
+		if node == nil {
+			result = append(result, "nil")
+			return
+		}
+
+		result = append(result, strconv.Itoa(node.Val))
+		traverse(node.Left)
+		traverse(node.Right)
+	}
+
+	traverse(root)
+
+	return strings.Join(result, ",")
+}
+
+// Deserializes your encoded data to tree.
+func (c *Codec) deserialize(data string) *TreeNode {
+	var traverse func() *TreeNode
+	values := strings.Split(data, ",")
+
+	i := 0
+	traverse = func() *TreeNode {
+		if string(values[i]) == "nil" {
+			i++
+			return nil
+		}
+
+		val, _ := strconv.Atoi(values[i])
+		node := &TreeNode{Val: val}
+		i++
+		node.Left = traverse()
+		node.Right = traverse()
+
+		return node
+	}
+
+	return traverse()
+}
