1762d5f15b9e67ef0ec26fe89a76d3f72e6b7eb1
manato kuroda <a7108100@gmail.com>
2022-11-26 09:03:12 +0900

    feat: add 42.TrappingRainWater.go

---
+++ questions/42.TrappingRainWater.go
@@ -0,0 +1,29 @@
+package questions
+
+import "math"
+
+func trap(height []int) int {
+	if len(height) == 0 {
+		return 0
+	}
+
+	left := 0
+	right := len(height) - 1
+	leftMax := height[left]
+	rightMax := height[right]
+	result := 0
+
+	for left < right {
+		if leftMax < rightMax {
+			left++
+			leftMax = int(math.Max(float64(leftMax), float64(height[left])))
+			result += leftMax - height[left]
+		} else {
+			right--
+			rightMax = int(math.Max(float64(rightMax), float64(height[right])))
+			result += rightMax - height[right]
+		}
+	}
+
+	return result
+}
