7788ec54dc86beb1c1d2d0668305f78b21a25490
manato kuroda <a7108100@gmail.com>
2022-12-12 08:37:08 +0900

    feat: add 128.LongestConsecutiveSequence.go

---
+++ questions/128.LongestConsecutiveSequence.go
@@ -0,0 +1,31 @@
+package questions
+
+import "math"
+
+func longestConsecutive(nums []int) int {
+	maps := make(map[int]struct{})
+	for _, num := range nums {
+		maps[num] = struct{}{}
+	}
+
+	result := 0
+	length := 0
+
+	for _, num := range nums {
+		_, ok := maps[num-1]
+		isStartSequence := !ok
+
+		if isStartSequence {
+			length = 0
+			for {
+				if _, ok = maps[num+length]; !ok {
+					break
+				}
+				length++
+			}
+			result = int(math.Max(float64(length), float64(result)))
+		}
+	}
+
+	return result
+}
