111a93ee224cc5420bd5bb1aaf9b78e0f08371fe
manato kuroda <a7108100@gmail.com>
2022-12-16 09:31:25 +0900

    feat: add 110.BalancedBinaryTree.go

--- questions/110.BalancedBinaryTree.go
+++ questions/110.BalancedBinaryTree.go
@@ -2,41 +2,35 @@ package questions
 
 import "math"
 
-type Treenode110 struct {
-	Val   int
-	Left  *Treenode110
-	Right *Treenode110
-}
-
-type Traverse110 struct {
+type Traverse struct {
 	Height     int
 	IsBalanced bool
 }
 
-func isBalanced(root *Treenode110) bool {
+func isBalanced(root *TreeNode) bool {
 	return traverse(root).IsBalanced
 }
-func traverse(node *Treenode110) Traverse110 {
+func traverse(node *TreeNode) Traverse {
 	if node == nil {
-		return Traverse110{Height: 0, IsBalanced: true}
+		return Traverse{Height: 0, IsBalanced: true}
 	}
 
 	left := traverse(node.Left)
 	right := traverse(node.Right)
 	b := left.IsBalanced && right.IsBalanced && math.Abs(float64(left.Height-right.Height)) <= 1
 
-	return Traverse110{
+	return Traverse{
 		IsBalanced: b,
 		Height:     1 + int(math.Max(float64(left.Height), float64(right.Height))),
 	}
 }
 
-//tree := &TreeNode_110{Val: 3}
-//tree.Left = &TreeNode_110{Val: 9}
+//tree := &TreeNode{Val: 3}
+//tree.Left = &TreeNode{Val: 9}
 //
-//tree.Right = &TreeNode_110{Val: 20}
-//tree.Right.Left = &TreeNode_110{Val: 15}
-//tree.Right.Right = &TreeNode_110{Val: 7}
+//tree.Right = &TreeNode{Val: 20}
+//tree.Right.Left = &TreeNode{Val: 15}
+//tree.Right.Right = &TreeNode{Val: 7}
 //
 //result := isBalanced(tree)
 //
