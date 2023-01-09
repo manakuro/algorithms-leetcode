42c50947fa5daef5536e4c8f3613ef16fc768e84
manato kuroda <a7108100@gmail.com>
2023-01-10 07:51:27 +0900

    feat: add 125.ValidPalindrome.ts

---
+++ leetcode/125.ValidPalindrome.ts
@@ -0,0 +1,16 @@
+function isPalindrome(s: string): boolean {
+  const formatted = s.replace(/[^a-z0-9]/gi, '').toLowerCase()
+  let left = 0
+  let right = formatted.length - 1
+
+  while (left < right) {
+    if (formatted[left] !== formatted[right]) return false
+    left++
+    right--
+  }
+
+  return true
+}
+
+console.log(isPalindrome('A man, a plan, a canal: Panama'))
+console.log(isPalindrome('a.'))
