4232b39e79997b93f76fb28fc3293617a4f995b5
manato kuroda <a7108100@gmail.com>
2023-02-01 08:25:25 +0900

    feat: add 53.MaximumSubarray.ts

---
+++ leetcode/53.MaximumSubarray.ts
@@ -0,0 +1,16 @@
+function maxSubArray(nums: number[]): number {
+  let max = nums[0]
+  let currentSum = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    if (currentSum < 0) {
+      currentSum = 0
+    }
+
+    currentSum += nums[i]
+    max = Math.max(max, currentSum)
+  }
+
+  return max
+}
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
