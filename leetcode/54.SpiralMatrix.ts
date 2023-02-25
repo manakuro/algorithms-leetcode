d17c415a2572c6658c71751a59d7273dd9a0af72
manato kuroda <a7108100@gmail.com>
2023-02-25 09:25:30 +0900

    feat: add 54.SpiralMatrix.ts

---
+++ leetcode/54.SpiralMatrix.ts
@@ -0,0 +1,43 @@
+function spiralOrder(matrix: number[][]): number[] {
+  const result = []
+  let left = 0
+  let right = matrix[0].length
+  let top = 0
+  let bottom = matrix.length
+
+  while (left < right && top < bottom) {
+    for (let i = left; i < right; i++) {
+      result.push(matrix[top][i])
+    }
+    top++
+
+    for (let i = top; i < bottom; i++) {
+      result.push(matrix[i][right - 1])
+    }
+    right--
+
+    // break if it reaches to the end
+    if (!(left < right && top < bottom)) break
+
+    // right to left in bottom row
+    for (let i = right - 1; i >= left; i--) {
+      result.push(matrix[bottom - 1][i])
+    }
+    bottom--
+
+    // bottom to top in left
+    for (let i = bottom - 1; i >= top; i--) {
+      result.push(matrix[i][left])
+    }
+    left++
+  }
+
+  return result
+}
+console.log(
+  spiralOrder([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ]),
+)
