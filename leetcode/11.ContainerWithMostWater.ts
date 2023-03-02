0c2f9365c41de1194487470e53ce692588dab9a5
manato kuroda <a7108100@gmail.com>
2023-03-03 08:24:54 +0900

    feat: add 11.ContainerWithMostWater.ts

---
+++ leetcode/11.ContainerWithMostWater.ts
@@ -0,0 +1,23 @@
+function maxArea(height: number[]): number {
+  let result = 0
+  let left = 0
+  let right = height.length - 1
+
+  while (left < right) {
+    const w = right - left
+    const h = Math.min(height[left], height[right])
+
+    const area = w * h
+    result = Math.max(result, area)
+
+    if (height[left] < height[right]) {
+      left++
+      continue
+    }
+
+    right--
+  }
+
+  return result
+}
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
