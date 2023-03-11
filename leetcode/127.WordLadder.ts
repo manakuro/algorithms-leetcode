641ab05399e55bf03d421f68cfdb5d88432eee9f
manato kuroda <a7108100@gmail.com>
2023-03-11 09:23:13 +0900

    feat: add 127.WordLadder.ts

---
+++ leetcode/127.WordLadder.ts
@@ -0,0 +1,50 @@
+const ladderLength = function (
+  beginWord: string,
+  endWord: string,
+  wordList: string[],
+) {
+  if (!wordList.includes(endWord)) return 0
+
+  const neighbors: Record<string, string[]> = {}
+
+  wordList.push(beginWord)
+
+  for (const word of wordList) {
+    for (let i = 0; i < word.length; i++) {
+      const pattern = word.slice(0, i) + '*' + word.slice(i + 1)
+      neighbors[pattern] ||= []
+      neighbors[pattern].push(word)
+    }
+  }
+
+  const visit = new Set()
+  visit.add([beginWord])
+
+  const queue: string[] = []
+  queue.push(beginWord)
+  let result = 1
+
+  while (queue.length) {
+    const length = queue.length
+    for (let i = 0; i < length; i++) {
+      const word = queue.shift()!
+      if (word === endWord) return result
+
+      for (let j = 0; j < word.length; j++) {
+        const pattern = word.slice(0, j) + '*' + word.slice(j + 1)
+        for (const neighbor of neighbors[pattern]) {
+          if (!visit.has(neighbor)) {
+            visit.add(neighbor)
+            queue.push(neighbor)
+          }
+        }
+      }
+    }
+    result++
+  }
+
+  return 0
+}
+console.log(
+  ladderLength('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log', 'cog']),
+)
