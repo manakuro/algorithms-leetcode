216dd94d862845cb47b77699787bfe25c5a7dfc7
manato kuroda <a7108100@gmail.com>
2023-03-14 08:38:29 +0900

    feat: add 84.LargestRectangleInHistogram.ts

---
+++ leetcode/84.LargestRectangleInHistogram.ts
@@ -0,0 +1,25 @@
+function largestRectangleArea(heights: number[]): number {
+  let max = 0
+  const stack: [number, number][] = []
+
+  for (let i = 0; i < heights.length; i++) {
+    const height = heights[i]
+    let start = i
+
+    while (stack.length && stack[stack.length - 1][1] > height) {
+      const [index, h] = stack.pop()!
+
+      max = Math.max(max, h * (i - index))
+      start = index
+    }
+
+    stack.push([start, height])
+  }
+
+  for (const [index, height] of stack) {
+    max = Math.max(max, height * (heights.length - index))
+  }
+
+  return max
+}
+console.log(largestRectangleArea([2, 1, 5, 6, 2, 3]))
