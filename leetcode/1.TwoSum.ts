7a710c2586e97beea0fc6be0f307fa3dd9b1bc5a
manato kuroda <a7108100@gmail.com>
2023-05-29 08:37:37 +0900

    feat: add twoSum

--- leetcode/1.TwoSum.ts
+++ leetcode/1.TwoSum.ts
@@ -12,3 +12,25 @@ function twoSum(nums: number[], target: number): number[] {
   return []
 }
 console.log(twoSum([2, 7, 11, 15], 9))
+
+function twoSum2(arr: number[], target: number): number[] {
+  let left = 0
+  let right = arr.length - 1
+
+  while (left < right) {
+    const sum = arr[left] + arr[right]
+    if (sum === target) {
+      return [left, right]
+    }
+
+    if (sum < target) {
+      left++
+    } else {
+      right--
+    }
+  }
+
+  return []
+}
+
+console.log(twoSum2([2, 7, 11, 15], 9))
